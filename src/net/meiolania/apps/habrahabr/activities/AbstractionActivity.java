aff4ca5685dd167c1dcee0eddee5a800df18bff6
Sergey Ivanov <si94.msk@gmail.com>
2012-10-23 01:12:30 +0400

    Update src/net/meiolania/apps/habrahabr/activities/AbstractionActivity.java

--- src/net/meiolania/apps/habrahabr/activities/AbstractionActivity.java
+++ src/net/meiolania/apps/habrahabr/activities/AbstractionActivity.java
@@ -24,6 +24,7 @@ import android.content.Context;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.view.WindowManager;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
